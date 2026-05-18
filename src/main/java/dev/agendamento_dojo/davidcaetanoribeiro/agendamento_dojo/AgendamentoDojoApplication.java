4ee0397606859b3f13cf8f539f9c314eb31ce811
David <davidcaetanoribeiro@gmail.com>
2026-05-18 16:58:44 -0300

    build: build inicial do projeto

---
+++ src/main/java/dev/agendamento_dojo/davidcaetanoribeiro/agendamento_dojo/AgendamentoDojoApplication.java
@@ -0,0 +1,13 @@
+package dev.agendamento_dojo.davidcaetanoribeiro.agendamento_dojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/agendamento_dojo/davidcaetanoribeiro/agendamento_dojo/AgendamentoDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.agendamento_dojo.davidcaetanoribeiro.agendamento_dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
