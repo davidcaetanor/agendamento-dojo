b1bdd1cadf0a4aceb5d32dd86b585ac77c03453c
David <davidcaetanoribeiro@gmail.com>
2026-06-18 23:35:28 -0300

    feat: adiciona UsuarioService para manipulação da busca do usuario por id, adiciona exception usuario nao encontrado, adiciona evento de UsuarioPromovido

---
+++ src/main/java/dev/agendamento_dojo/davidcaetanoribeiro/agendamento_dojo/event/UsuarioPromovidoProfessorEvent.java
@@ -0,0 +1,6 @@
+package dev.agendamento_dojo.davidcaetanoribeiro.agendamento_dojo.event;
+
+import java.util.UUID;
+
+public record UsuarioPromovidoProfessorEvent(UUID idUsuario) {
+}
---
+++ src/main/java/dev/agendamento_dojo/davidcaetanoribeiro/agendamento_dojo/exceptions/UsuarioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package dev.agendamento_dojo.davidcaetanoribeiro.agendamento_dojo.exceptions;
+
+public class UsuarioNaoEncontradoException extends RuntimeException {
+    public UsuarioNaoEncontradoException(String message) {
+        super(message);
+    }
+}
